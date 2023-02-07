3fd3654c902c7e05c56d7344d46dbc3ad1926bd4
neelapalavamsikrishna <124710416+neelapalavamsikrishna@users.noreply.github.com>
2023-02-07 22:30:42 +0530

    Add files via upload

---
+++ stringpackage/string1.java
@@ -0,0 +1,10 @@
+package stringpackage;
+
+public class string1 {
+    public static void main(String[] args) {
+        String name = "javatpoint";
+        char ch = name.charAt(4);
+        System.out.println(ch);
+    }
+}
+
